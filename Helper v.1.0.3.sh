c6bdd4b2b2d13ea1ef9f8ad35f7b1b4e11b712e2
Nico-Shock <155913683+Nico-Shock@users.noreply.github.com>
2025-01-05 18:25:12 +0100

    Update Helper v.1.0.3.sh

--- Helper v.1.0.3.sh
+++ Helper v.1.0.3.sh
@@ -44,14 +44,14 @@ install_dolphin=false
 install_gnome_tweaks=false
 
 clear
-echo -e "${blue}Welcome to the Arch Linux post installation script!${reset}"
+echo -e "${blue}Welcome to my Arch Linux post installation script!${reset}"
 
 ask_user "Do you want to install the CachyOS repos?" install_cachyos
 ask_user "Do you want to install the Chaotic-AUR-repos?" install_chaotic
 ask_user "Do you want to install the CachyOS Kernel Manager?" install_kernel_manager
 ask_user "Do you want to install the CachyOS Gaming Meta? (Proton, Steam, Lutris, Heroic Games Launcher, Wine – idk the exact package names, sorry)" install_gaming_meta
-ask_user "Do you you want to install Nvidia open drivers?" install_open_nvidia_driver
-ask_user "Do you want to install Nvidia closed source drivers?" install_closed_nvidia_dkms_driver
+ask_user "Do you want to install Nvidia open drivers? (linux-cachyos-nvidia-open, libglvnd, nvidia-utils, opencl-nvidia, lib32-libglvnd, lib32-nvidia-utils, lib32-opencl-nvidia, nvidia-settings)" install_open_nvidia_driver
+ask_user "Do you want to install Nvidia closed source drivers? (nvidia-dkms, libglvnd, nvidia-utils, opencl-nvidia, lib32-libglvnd, lib32-nvidia-utils, lib32-opencl-nvidia, nvidia-settings, linux-headers)" install_closed_nvidia_dkms_driver
 ask_user "Do you want to install recommended software? (yay, ufw, fzf, python, python-pip, bluez, blueman, bluez-utils, zram-generator, fastfetch, preload, flatpak, git, wget, gedit, thermald)" install_recommended_software
 
 echo -e "${blue}Do you use KDE or Gnome? [k/g/n]:${reset}"
@@ -64,19 +64,21 @@ elif [[ $desktop_env =~ ^[Gg]$ ]]; then
 fi
 
 if $install_cachyos; then
-  download_and_install "https://mirror.cachyos.org/cachyos-repo.tar.xz" "cachyos-repo.tar.xz"
-  cd cachyos-repo
-  sudo ./cachyos-repo.sh
-  cd .. && rm -rf cachyos-repo cachyos-repo.tar.xz
+  wget https://mirror.cachyos.org/cachyos-repo.tar.xz &&
+  tar xvf cachyos-repo.tar.xz &&
+  cd cachyos-repo &&
+  sudo ./cachyos-repo.sh &&
+  cd .. &&
+  rm -rf cachyos-repo cachyos-repo.tar.xz &&
   sudo pacman -S --noconfirm cachyos-settings
 fi
 
 if $install_chaotic; then
-  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-  sudo pacman-key --lsign-key 3056513887B78AEB
-  sudo pacman -U --noconfirm https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst
-  sudo pacman -U --noconfirm https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
-  grep -q 'chaotic-aur' /etc/pacman.conf || echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
+  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com &&
+  sudo pacman-key --lsign-key 3056513887B78AEB &&
+  sudo pacman -U --noconfirm https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst &&
+  sudo pacman -U --noconfirm https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst &&
+  grep -q 'chaotic-aur' /etc/pacman.conf || echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf &&
   sudo pacman -Sy
 fi
 
